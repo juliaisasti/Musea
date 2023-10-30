1a59049009026121321bb36fdefc0249cd9655f7
juliaisasti <144235263+juliaisasti@users.noreply.github.com>
2023-10-30 12:48:54 +0100

    basic structured created, API fetch added

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+//Fetch de los objetos del museo
 async function getObject() {
     try {
         let response = await fetch ("https://collectionapi.metmuseum.org/public/collection/v1/objects");
@@ -9,3 +11,22 @@ async function getObject() {
 }
 
 getObject().then((objectsData) => console.log(objectsData));
+
+// Función para obtener imagen random para el slider cada vez que se refresca la página
+async function getSliderImg() {
+    let id = Math.floor(Math.random() * 500)
+    try {
+        let response2 = await fetch (`https://collectionapi.metmuseum.org/public/collection/v1/objects/${id}`);
+        let objectsImgData = await response2.json();
+        let img = objectsImgData.primaryImage;
+        let name = objectsImgData.title;
+        document.getElementById("photoSlider").innerHTML = `<section>
+        <img src="${img}" alt="${name}"
+        <h1>${name}</h1>
+        </section>`;
+    } catch (error) {
+        console.log("Error");
+    }
+}
+
+getSliderImg();
