21479580f16a6756f03d2972d9e9834a3be1ba72
juliaisasti <144235263+juliaisasti@users.noreply.github.com>
2023-11-01 20:36:08 +0100

    font change, z-index added

--- script.js
+++ script.js
@@ -14,15 +14,15 @@
 
 // Función para obtener imagen random para el slider cada vez que se refresca la página
 async function getSliderImg() {
-    let id = Math.floor(Math.random() * 500)
+    let id = Math.floor(Math.random() * 400000)
     try {
         let response2 = await fetch (`https://collectionapi.metmuseum.org/public/collection/v1/objects/${id}`);
         let objectsImgData = await response2.json();
         let img = objectsImgData.primaryImage;
         let name = objectsImgData.title;
+        console.log(objectsImgData);
         document.getElementById("photoSlider").innerHTML = `<section>
         <img id="photoSlider" src="${img}" alt="${name}"
-        <h1>${name}</h1>
         </section>`;
     } catch (error) {
         console.log("Error");
