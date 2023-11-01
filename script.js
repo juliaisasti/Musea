30d6fbfa121bb450a73435215ae4d2b444438cf3
juliaisasti <144235263+juliaisasti@users.noreply.github.com>
2023-11-01 19:34:43 +0100

    CSS fixed, API Objects are shown on homepage

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 
-//Fetch de los objetos del museo
-async function getObject() {
-    try {
-        let response = await fetch ("https://collectionapi.metmuseum.org/public/collection/v1/objects");
-        let objectsData =await response.json();
-        return objectsData;
-    } catch (error) {
-        console.log("Error");
-    }
-}
+// //Fetch de los objetos del museo
+// async function getObject() {
+//     try {
+//         let response = await fetch ("https://collectionapi.metmuseum.org/public/collection/v1/objects");
+//         let objectsData =await response.json();
+//         return objectsData;
+//     } catch (error) {
+//         console.log("Error");
+//     }
+// }
 
-getObject().then((objectsData) => console.log(objectsData));
+// getObject().then((objectsData) => console.log(objectsData));
 
 // Función para obtener imagen random para el slider cada vez que se refresca la página
 async function getSliderImg() {
@@ -21,7 +21,7 @@ async function getSliderImg() {
         let img = objectsImgData.primaryImage;
         let name = objectsImgData.title;
         document.getElementById("photoSlider").innerHTML = `<section>
-        <img src="${img}" alt="${name}"
+        <img id="photoSlider" src="${img}" alt="${name}"
         <h1>${name}</h1>
         </section>`;
     } catch (error) {
